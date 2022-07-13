381f07919e789dbe59819195fee470e3d0e365bf
NMSamDev <c_s_m_h_13@hotmail.com>
2022-07-13 14:59:26 -0400

    first commit

---
+++ app/src/main/java/com/example/carlosmendez_assignment_2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.carlosmendez_assignment_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.carlosmendez_assignment_2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+    }
+}
---
+++ app/src/main/java/com/example/carlosmendez_assignment_2/SongResponse.kt
@@ -0,0 +1,18 @@
+package com.example.carlosmendez_assignment_2
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class SongResponse(
+    val results: List<Song>
+) : Parcelable
+
+@Parcelize
+data class Song(
+    val artistName: String,
+    val collectionName: String,
+    val artworkUrl60: String,
+    val trackPrice: String
+) : Parcelable
+
